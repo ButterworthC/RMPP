0ffe90445460feeeb57888cef041f1b9a22ac966
ButterworthC <cbutterw@hotmail.com>
2025-01-28 20:24:41 +0000

    first commit

---
+++ create_repo.bat
@@ -0,0 +1,20 @@
+rem this was created by ChatGPT
+rem use it for the next module (if it bloody works)
+
+cd "C:\COLLEGE\Artificial Intelligence PGDip\Research Methods and Professional Practice\RMPP"
+
+rem Run the following command to initialize a Git repository in that folder:
+git init
+rem This will create a hidden .git folder in your RMPP directory, making it a Git repository.
+
+rem Add the GitHub repository as the remote by running:
+git remote add origin https://github.com/ButterworthC/RMPP.git
+
+rem Stage all the files in your folder for commit by running:
+git add .
+
+rem Commit your changes with a message:
+git commit -m "Initial commit of RMPP project"
+
+rem Finally, push the changes to the main branch on GitHub:
+git push origin main
